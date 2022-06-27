3b1e47f8ae972ac356a78f602ce6bbe4fd9428fa
souvik-wizard <dassouvik3327@gmail.com>
2022-06-27 22:56:04 +0530

    ana update

---
+++ anagram.py
@@ -0,0 +1,21 @@
+string1 = "Listen"
+string2 = "Silent"
+
+string1 = string1.lower()
+string2 = string2.lower()
+
+leng1=len(string1)
+leng2=len(string2)
+
+
+if( leng1 == leng2):
+   
+    sorting1=sorted(string1)
+    sorting2=sorted(string2)
+
+    if sorting1 == sorting2:
+        print("Are anagram")
+    else:
+        print("Are not anagram")
+else:
+    print("Not matching")
