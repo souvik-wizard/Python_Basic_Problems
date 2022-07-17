6e6d12026aeed94b7e01a1ce67556510d3b8cbaf
souvik-wizard <dassouvik3327@gmail.com>
2022-07-17 18:50:56 +0530

    Update Finding square root .py

--- Finding square root .py
+++ Finding square root .py
@@ -1,4 +1,4 @@
-                  
+#  Leetcode Problem Number 69                 
 # 1
 num = float(input('Enter a number: ')) 
             #  **means num is multiplying 0.5(here) times
@@ -21,4 +21,5 @@ print("The square root of {0} is: {1} ".format(num,float_val))
 import math
 num = float(input('Enter a number: ')) 
 root=math.sqrt(num)
+print(math.floor(root))
 print("The square root of {0} is: {1} ".format(num,root))
