b08b61c7000d1407439675ad6f08d1563e3c1cc3
souvik-wizard <dassouvik3327@gmail.com>
2022-08-01 19:38:39 +0530

    .

--- prime_num_check.py
+++ prime_num_check.py
@@ -1,37 +1,63 @@
-num=int(input("Enter the number:"))
-if num>1 :
-    for i in range(2,num):
-       if num%i==0:
-                print("This is not a prime number, because ",i,"times",num//i,"is",num)
-                break
-    else:
-        print(num," is a prime number.")
-else:
-    print("If input number is less than or equal to 1, it is not prime!")
+# num=int(input("Enter the number:"))
+# if num>1 :
+#     for i in range(2,num):
+#        if num%i==0:
+#                 print("This is not a prime number, because ",i,"times",num//i,"is",num)
+#                 break
+#     else:
+#         print(num," is a prime number.")
+# else:
+#     print("If input number is less than or equal to 1, it is not prime!")
+
+# # -----------------------------
+
+# c=0
+# for i in range(1,20):
+#     for j in range(2,i):
+#         if i%j==0:
+#             c+=1
+#             break
+            
+# print(c)
+
 
 # -----------------------------
+# nums=499979
+# # print("Prime numbers between", lower, "and", upper, "are:")
 
-
-for i in range(1,20):
-    for j in range(2,i):
-        if i%j==0:
-            break
-    print(i)
-
-
-# -----------------------------
-lower = 0
-upper = 20
-
-# print("Prime numbers between", lower, "and", upper, "are:")
-
-
-for i in range(lower, upper+1):
-    if i > 2:
-        for j in range(2, i):
-            if i % j == 0:
-                break
-        else:
-            print(i)
+# c=0
+# for i in range(nums):
+#     if i > 1:
+#         for j in range(2, i):
+#             if i % j == 0:
+                
+#                 break
+#         else:
+#             c+=1
+# print(c)
 
     
+
+    # Python program to print all
+# prime number in an interval
+
+def prime(n):
+	prime_list = []
+	for i in range(n):
+		if i == 0 or i == 1:
+			continue
+		else:
+			for j in range(2, int(i/2)+1):
+				if i % j == 0:
+					break
+			else:
+				prime_list.append(i)
+	return prime_list
+
+# Driver program
+n=4999799
+lst = prime(n)
+if len(lst) == 0:
+	print("There are no prime numbers in this range")
+else:
+	print("The prime numbers in this range are: ", len(lst))
---
+++ set_concept.py
@@ -0,0 +1,10 @@
+# a set cannot  contain same numbers .
+# if a set includes [1,2,3,2,1,5,5,5] then it will only dislay ['1','2','3',5] only NO DUPLICATES
+
+# Examples
+
+s1 = {"egg"}
+print(set(s1))
+
+s2=[1,2,3,2,1,5,5,5]
+print(set(s2))
